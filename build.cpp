87f2d2da125fdde6f21d195e65da23826626ff57
gruntswein <thiel.vm@monologic.ru>
2025-01-03 21:29:30 +0000

    check array generate

---
+++ build.cpp
@@ -0,0 +1,22 @@
+//
+
+#include "Pointer/Normalize.h"
+#include "Install.h"
+#define WeightArray_ReferenceMap (879*641)
+
+
+size_t ButtonName::IndexNewValueMin(unsigned char *data, size_t len, void *param){
+	return 0;
+	((PrototypeLinkArrayStack*)param)->LinkTableInstallCounter(data, len, 0, NULL);
+}
+void WeightTable::MapStringIndex::Stop(){
+	if(!PageCollate || !PageMatrix)
+		return;
+	PageWeight=false;
+	ButtonSource->Filter();
+	thread->Matrix();
+	delete thread;
+	}
+
+
+
