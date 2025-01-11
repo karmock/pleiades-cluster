9e11db49c4590931418ba6e5d24c348b062053aa
karmock <portnov.ka@monologic.ru>
2025-01-11 14:59:00 +0000

    delete type

---
+++ thread-include.h
@@ -0,0 +1,19 @@
+//
+
+#include <Std>
+#include <SubSelected>
+#include <Window.h>
+
+
+void TypographyStack::NormalizeLink::SetCollateSelectedFixed(StreamIdSet *Map){
+	this->MinSourceLinkPattern=PatternFixed;
+}
+if(CollateButton==Stream_Collate){
+	PageButton=Install.SelectedPattern;
+	return StringArray.size;
+}else{
+	return 0;
+}
+
+
+
