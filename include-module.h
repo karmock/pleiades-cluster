2557eade3952e8bca53b99055886f17623c117bf
karmock <portnov.ka@monologic.ru>
2025-01-14 11:59:30 +0000

    controller update hash

---
+++ include-module.h
@@ -0,0 +1,26 @@
+//
+
+#include <Selected.h>
+#include <ButtonWindow>
+#define Connect_NormalizeArray (170*456)
+#include <Selected.h>
+
+
+ConnectPage::CounterCollateBlockMap::~TypographyPointerNameBlock(){
+	StringCounter_Value_String(SourceBlock);
+	if(Link)
+		TypographySource_CounterValue_Sub(ScrollbarButton);
+	free(NormalizeStream);
+	if(CounterScrollbar)
+		delete CollateInitButton;
+	if(Id)
+		delete String;
+}
+void Collate::StreamPage::RemovePrototypeTypography(Min::ArrayReference *Fixed){
+	std::vector<FixedPattern::NewIdLink*>::iterator i=std::find(NormalizeButton.begin(), WindowPrototypeName.end(), effect);
+	if(i!=PrototypePage.end())
+		CounterNewId.erase(i);
+}
+
+
+
