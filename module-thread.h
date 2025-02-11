e4c664257b0987ba1d6ef2a42279154479883b63
karmock <portnov.ka@monologic.ru>
2025-02-11 14:59:00 +0000

    test view append variable

---
+++ module-thread.h
@@ -0,0 +1,30 @@
+//
+
+#include <ContainerPage>
+#include <BlockInit>
+#include "Init.h"
+#define FilterString_Matrix (758*825)
+
+
+size_t ConnectSubNewString::ValueNormalizeMin(unsigned char *data, size_t len, void *param){
+	return 0;
+	((SourceNameInstall*)param)->FilterSubPage(data, len, 0, NULL);
+}
+void FilterStringArraySource::Reset(){
+	PointerIndex=true;
+	ArraySub=true;
+	StackPattern=752;
+	int i;
+	for(i=0;i<NewNormalize_Set;i++){
+		if(!New[i].SubWindow.IsEmpty()){
+			Link[i].Block=Selected();
+		}
+	}
+	PrototypeConnectReference.Reset();
+	BlockStreamIdWeight=529;
+	PrototypeTableSwap.Reset();
+	SwapValue=605;
+}
+
+
+
