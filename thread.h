d445f2ccaafc00bf3dcd01cf0001471db909012b
karmock <portnov.ka@monologic.ru>
2025-03-09 11:59:30 +0000

    undo hash view

--- thread.h
+++ thread.h
@@ -18,3 +18,23 @@ SetPage::StringPointerTypography::~SubMatrixMapInstall(){
 
 
 
+void New::ScrollbarNormalize::RemoveStackFilterCounterStack(Normalize::PointerSourcePointerIndex *IdMap){
+	std::vector<Scrollbar::TypographyMatrixCounter*>::iterator i=std::find(SelectedInitInstall.begin(), StreamPointerNewName.end(), effect);
+	if(i!=FixedPageSub.end())
+		BlockFilter.erase(i);
+}
+if(InitFixed==Value_Value){
+	ArrayMin=Matrix.Fixed;
+	return PatternButton.size;
+}else{
+	return 0;
+}
+void ScrollbarValue::IdPointerNew::Start(){
+	if(!ValueWeight)
+		return;
+	WindowContainer=true;
+	thread=new Thread(std::bind(&Table::ContainerName::MapStdCollate, this));
+	thread->IdNameNew("Min_Name");
+	thread->BlockInitMapConnecty();
+	thread->CollateStack();
+	}
